e0299558e2587b073a869263603d247fbac9f025
Hilaire Bekono <mbhd90@gmail.com>
2022-04-11 19:15:56 +0200

    Update logo

--- src/app/all-modules/chat/signup-email/signup-email.component.ts
+++ src/app/all-modules/chat/signup-email/signup-email.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AllModulesService } from 'src/app/all-modules.service';
-import { Validations } from 'src/app/utils/validations';
+import Validations from 'src/app/utils/validations';
 
 @Component({
   selector: 'app-signup-email',
@@ -10,26 +10,25 @@ import { Validations } from 'src/app/utils/validations';
 })
 export class SignupEmailComponent implements OnInit {
   public form: FormGroup;
-  public firstname = '';
+  public firstName = '';
   public lastname = '';
   public username = '';
   public confirmPassword = '';
   public password = '';
-  public acceptTerms:boolean = true;
+  public acceptTerms:boolean = false;
   public loading:boolean = false;
   submitted = false;
-
 
   constructor(protected readonly allModulesService: AllModulesService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      firstname: new FormControl(this.firstname, []),
+      firstName: new FormControl(this.firstName, []),
       lastname: new FormControl(this.lastname, [Validators.required]),
       username: new FormControl(this.username, [Validators.required, Validators.email]),
       password: new FormControl(this.password, [Validators.required]),
-      confirmPassword: new FormControl(this.confirmPassword, [Validators.required]),
-      acceptTerms: new FormControl(this.acceptTerms, [Validators.required])
+      confirmPassword: new FormControl(this.confirmPassword, [Validators.required, Validations.match("password")]),
+      acceptTerms: new FormControl(this.acceptTerms, [Validators.requiredTrue])
     });
   }
 
--- src/app/utils/validations.ts
+++ src/app/utils/validations.ts
@@ -1,18 +1,22 @@
-import { AbstractControl } from '@angular/forms';
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
-
-export class Validations {
-
-    static match(a: any, b:any) {
-        console.log(a);
-        console.log(b);
-        return null;
-    }
-
-    static requiredTrue(control: AbstractControl) {
-        if (control.value == true) {
-          return { urlValid: true };
+export default class Validation {
+    static match(matchingControlName: string): ValidatorFn {
+        return (control:AbstractControl) : ValidationErrors | null => {
+                if(control.parent == null){
+                    return;
+                }
+                const matchingControl = control.parent.controls[matchingControlName];
+                if (control.errors && !control.errors.mustMatch) {
+                    // return if another validator has already found an error on the matchingControl
+                    return;
+                }
+                // set error on matchingControl if validation fails
+                if (control.value !== matchingControl.value) {
+                    return { mustMatch: true };
+                } else {
+                    return null;
+                }
         }
-        return null;
     }
 }
